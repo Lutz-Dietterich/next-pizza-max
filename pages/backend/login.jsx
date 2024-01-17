b22409144f98ef5a00fba4c9b12a1ac322329d8a
Lutz Dietterich <lutzdietterich@googlemail.com>
2024-01-17 13:26:31 +0100

    change root directory3

--- pages/backend/login.jsx
+++ pages/backend/login.jsx
@@ -11,13 +11,10 @@ export default function Login() {
 
   const login = async () => {
     try {
-      await axios.post(
-        "https://next-pizza-max.lutz-dietterich.vercel.app/api/login",
-        {
-          benutzer,
-          passwort,
-        }
-      );
+      await axios.post("https://next-pizza-max.vercel.app/api/login", {
+        benutzer,
+        passwort,
+      });
       router.push("/backend");
     } catch (error) {
       setError(true);
